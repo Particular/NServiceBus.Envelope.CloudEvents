fd8f8747d5d22338f1d002481e790adaaffd4ec6
Mauro Servienti <mauro.servienti@gmail.com>
2025-11-24 18:02:11 +0100

    basic setup Infrastructure

---
+++ src/NServiceBus.Envelope.CloudEvents/CloudEventsEndpointConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus;
+
+using Configuration.AdvancedExtensibility;
+
+public static class CloudEventsEndpointConfigurationExtensions
+{
+    internal const string CloudEventsSetting = "NServiceBus.Envelope.CloudEvents";
+    
+    public static CloudEventsConfiguration EnableCloudEvents(this EndpointConfiguration configuration)
+    {
+        var config = new CloudEventsConfiguration();
+        var settings = configuration.GetSettings();
+        settings.Set(CloudEventsSetting, config);
+        
+        return config;
+    }
+}
