6424dc2d50e6e9d97991361e3289af54c4adf610
Mauro Servienti <mauro.servienti@gmail.com>
2025-11-27 17:14:51 +0100

    Add the new handlers to DI and diagnostics

--- src/NServiceBus.Envelope.CloudEvents/CloudEventsFeature.cs
+++ src/NServiceBus.Envelope.CloudEvents/CloudEventsFeature.cs
@@ -9,13 +9,17 @@ class CloudEventsFeature : Feature
     protected override void Setup(FeatureConfigurationContext context)
     {
         context.Services.AddSingleton<IEnvelopeHandler, CloudEventJsonStructuredEnvelopeHandler>();
+        context.Services.AddSingleton<IEnvelopeHandler, CloudEventAmqpBinaryEnvelopeHandler>();
+        context.Services.AddSingleton<IEnvelopeHandler, CloudEventHttpBinaryEnvelopeHandler>();
 
         context.Settings.AddStartupDiagnosticsSection("NServiceBus.Envelope.CloudEvents",
             new
             {
                 RegisteredEnvelopeHandlers = new[]
                 {
-                    nameof(CloudEventJsonStructuredEnvelopeHandler)
+                    nameof(CloudEventJsonStructuredEnvelopeHandler),
+                    nameof(CloudEventAmqpBinaryEnvelopeHandler),
+                    nameof(CloudEventHttpBinaryEnvelopeHandler)
                 },
                 // TODO list here all the settings from CloudEventsConfiguration
                 Configuration = ""
