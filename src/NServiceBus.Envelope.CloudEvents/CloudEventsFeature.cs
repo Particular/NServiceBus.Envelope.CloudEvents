a4b2579008b78b583f8da695621c043f20289088
Mauro Servienti <mauro.servienti@gmail.com>
2025-11-27 16:12:18 +0100

    Add the CloudEventsFeature to register supported envelope handlers in DI

---
+++ src/NServiceBus.Envelope.CloudEvents/CloudEventsFeature.cs
@@ -0,0 +1,13 @@
+namespace NServiceBus.Envelope.CloudEvents;
+
+using Features;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+class CloudEventsFeature : Feature
+{
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        context.Services.AddSingleton<CloudEventJsonStructuredEnvelopeHandler>();
+    }
+}
